a649b4c37807889e279882f9c96323aebec7c276
Bill Strait <bill.strait@gmail.com>
2012-04-12 19:01:52 -0500

    first commit

---
+++ Notes/Notes/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Notes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTrouble = new System.Windows.Forms.TextBox();
+            this.txtResolution = new System.Windows.Forms.TextBox();
+            this.drpPrefabs = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtIssue = new System.Windows.Forms.TextBox();
+            this.chkSpell = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Issue";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Troubleshooting Steps";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 423);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resolution";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Prefabs:";
+            // 
+            // txtTrouble
+            // 
+            this.txtTrouble.Location = new System.Drawing.Point(13, 199);
+            this.txtTrouble.Multiline = true;
+            this.txtTrouble.Name = "txtTrouble";
+            this.txtTrouble.Size = new System.Drawing.Size(527, 221);
+            this.txtTrouble.TabIndex = 3;
+            // 
+            // txtResolution
+            // 
+            this.txtResolution.Location = new System.Drawing.Point(13, 440);
+            this.txtResolution.Multiline = true;
+            this.txtResolution.Name = "txtResolution";
+            this.txtResolution.Size = new System.Drawing.Size(527, 138);
+            this.txtResolution.TabIndex = 4;
+            // 
+            // drpPrefabs
+            // 
+            this.drpPrefabs.FormattingEnabled = true;
+            this.drpPrefabs.Items.AddRange(new object[] {
+            "<nothing>",
+            "Aircard Setup",
+            "Android Email Setup",
+            "Beyond Trust Repair",
+            "Blackberry Activation",
+            "ESS Portal Prompting for Password",
+            "Guest Wireless Password",
+            "HP 460/470 Installation",
+            "Windows Password"});
+            this.drpPrefabs.Location = new System.Drawing.Point(13, 30);
+            this.drpPrefabs.Name = "drpPrefabs";
+            this.drpPrefabs.Size = new System.Drawing.Size(121, 21);
+            this.drpPrefabs.TabIndex = 1;
+            this.drpPrefabs.SelectedIndexChanged += new System.EventHandler(this.drpPrefabs_SelectedIndexChanged);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(465, 604);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(384, 603);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtIssue
+            // 
+            this.txtIssue.Location = new System.Drawing.Point(13, 90);
+            this.txtIssue.Multiline = true;
+            this.txtIssue.Name = "txtIssue";
+            this.txtIssue.Size = new System.Drawing.Size(527, 89);
+            this.txtIssue.TabIndex = 2;
+            // 
+            // chkSpell
+            // 
+            this.chkSpell.Location = new System.Drawing.Point(13, 604);
+            this.chkSpell.Name = "chkSpell";
+            this.chkSpell.Size = new System.Drawing.Size(87, 23);
+            this.chkSpell.TabIndex = 7;
+            this.chkSpell.Text = "Check Spelling";
+            this.chkSpell.UseVisualStyleBackColor = true;
+            this.chkSpell.Click += new System.EventHandler(this.chkSpell_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 639);
+            this.Controls.Add(this.chkSpell);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.drpPrefabs);
+            this.Controls.Add(this.txtResolution);
+            this.Controls.Add(this.txtTrouble);
+            this.Controls.Add(this.txtIssue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SJM Service Desk Note Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTrouble;
+        private System.Windows.Forms.TextBox txtResolution;
+        private System.Windows.Forms.ComboBox drpPrefabs;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtIssue;
+        private System.Windows.Forms.Button chkSpell;
+    }
+}
+
---
+++ Notes/Notes/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using Word = Microsoft.Office.Interop.Word;
+using System.Text.RegularExpressions;
+
+
+
+
+namespace Notes
+{
+    public partial class Form1 : Form
+    {       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drpPrefabs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (drpPrefabs.SelectedIndex)
+            {
+                case 0:
+                    //do nothing, the value is set to null.
+                    break;
+                case 1:
+                    //aircard setup
+                    txtIssue.Text = "Client has a new air card which needs to be configured.";
+                    txtTrouble.Text = "Walked client through seperating the air card from the adaptor. Assisted them with locating the proper port on the right side of their PC. Ran Wizard in VZ Access Manager, configured to use PC card.";
+                    txtResolution.Text = "Connected to the internet via VZ Access Manager.";
+                    break;
+                case 2:
+                    //android email setup
+                    txtIssue.Text = "Client has a new Android based cell phone, and they would like it to be configured for SJM email.";
+                    txtTrouble.Text = "Walked client through configuring Android email following the documented procedure.";
+                    txtResolution.Text = "Verified emails were synchronized to the phone.";
+                    break;
+                case 3:
+                    //Beyond Trust Repair
+                    txtIssue.Text = "The 'PM - Run Elevated...' option is missing when right clicking on applications.";
+                    txtTrouble.Text = "Ran deleted the Beyond Trust Channel. Rebooted the PC. Updated policy service channel, verified Beyond Trust installed again. Rebooted pc.";
+                    txtResolution.Text = "Right clicked an executable, 'PM - Run Elevated...' was present.";
+                    break;
+                case 4:
+                    //Blackberry Activation
+                    txtIssue.Text = "Client has a new blackberry that needs to be activated.";
+                    txtTrouble.Text = "Verified user following the standard password reset policy. Set activation Password. Walked client through activating the phone.";
+                    txtResolution.Text = "Verified contacts and calendars loaded onto the phone.";
+                    break;
+                case 5:
+                    //ESS Portal Prompts for Password
+                    txtIssue.Text = "When accessing the ESS portal the user is prompted for a user name and password.";
+                    txtTrouble.Text = "Had client close all open Internet Explorer windows. Instructed the user to right click on 'Secude' and choose 'Enroll for Default.'";
+                    txtResolution.Text = "Had client access https://ess.sjm.com, verified it prompted for the last four of their Social Security Number.";
+                    break;
+                case 6:
+                    //Guest Wireless Password
+                    txtIssue.Text = "User is requesting the guest wifi password.";
+                    txtTrouble.Text = "Provided user with the guest wifi password. Offered to assist with logging in to the guest network.";
+                    txtResolution.Text = "Verified client was able to access the network.";
+                    break;
+                case 7:
+                    //HP 460/470 Installation
+                    txtIssue.Text = "User is trying to set up their SJM issued printer.";
+                    txtTrouble.Text = "Remoted to user's PC, configured bluetooth to connect to the printer using drivers already installed on the system. Had client plug printer in via USB.";
+                    txtResolution.Text = "Printed test page over bluetooth.";
+                    break;
+                case 8:
+                    //Windows Password
+                    txtIssue.Text = "User needs their NT account password reset.";
+                    txtTrouble.Text = "Verified the user following the standard password reset policy. Reset the password in DRA.";
+                    txtResolution.Text = "Verified user was able to log in and reset their password.";
+                    break;
+            }
+
+        }
+
+        //copy data to the clipboard and reset the form
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            string dataToBeCopied = "Issue:\n" + txtIssue.Text.ToString() + "\n\nTroubleshooting:\n" + txtTrouble.Text.ToString() + "\n\nResolution:\n" + txtResolution.Text.ToString();
+            Clipboard.SetText(dataToBeCopied);
+            reset();
+        }
+
+        //manually reset the form
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        //reset the form
+        private void reset()
+        {
+            drpPrefabs.SelectedItem = 0;
+            txtIssue.Text = "";
+            txtTrouble.Text = "";
+            txtResolution.Text = "";
+        }
+
+
+        private void chkSpell_Click(object sender, EventArgs e)
+        {
+            Word.Application app = new Word.Application();
+
+            int errors = 0;
+
+            //don't show word doing all the work
+            app.Visible = false;
+
+            //setting variables that are equivalent to null.
+            object template = Missing.Value;
+            object newTemplate = Missing.Value;
+            object documentType = Missing.Value;
+            object visible = true;
+
+            //initalize a document
+            Word._Document doc1 = app.Documents.Add(ref template, ref newTemplate, ref documentType, ref visible);
+
+            //a counter token
+            int i = 0;
+
+            //a variable to hold the output
+            string[] holder = Regex.Split(txtIssue.Text, "\r\n");
+            
+
+            while (holder.Length > i)
+            {
+                doc1.Words.First.InsertBefore(holder[i]);
+
+                Word.ProofreadingErrors spellErrorsColl = doc1.SpellingErrors;
+                errors = spellErrorsColl.Count;
+
+                object optional = Missing.Value;
+
+                doc1.CheckSpelling(ref optional, ref optional, ref optional, ref optional, ref optional, ref optional, ref optional, ref optional, ref optional, ref optional, ref optional, ref optional);
+
+                object first = 0;
+                object last = doc1.Characters.Count - 1;
+
+                holder[i] = doc1.Range(ref first, ref last).Text;
+                Word.Range deleteme = doc1.Range(ref first, ref last);
+                deleteme.Delete(ref optional, ref optional);
+
+                i = i+1;
+            }
+            txtIssue.Text = "";
+            foreach (string line in holder){
+                txtIssue.Text += line + "\r\n";
+            }
+
+            
+
+            object saveChanges = false;
+            object originalFormat = Missing.Value;
+            object routeDocument = Missing.Value;
+
+            app.Quit(ref saveChanges, ref originalFormat, ref routeDocument);
+            
+        }
+    }
+}
